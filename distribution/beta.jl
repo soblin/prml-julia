5d3e3a0e6a9a13448577dab450466cf8c7452fde
soblin <hilo.soblin@gmail.com>
2020-06-17 15:31:16 +0900

    [ch2]visualized beta dist

--- distribution/beta.jl
+++ distribution/beta.jl
@@ -1,12 +1,25 @@
 module beta_dist
 
+using SpecialFunctions
+
 export BetaDist
 export pdf
 
 mutable struct BetaDist
-    _n_ones::Int64
-    _n_zeros::Int64
+    _n_ones::Float64
+    _n_zeros::Float64
 end
 
-function pdf(beta::BetaDist, )
+function pdf(beta::BetaDist, mu::Float64)
+    a = beta._n_ones;
+    b = beta._n_zeros;
+    return gamma(a + b) / (gamma(a) * gamma(b)) * mu^(a-1.0) * (1.0 - mu)^(b-1.0)
+end
+
+function pdf(beta::BetaDist, mu::Array{Float64, 1})
+    a = beta._n_ones;
+    b = beta._n_zeros;
+    return gamma(a + b) / (gamma(a) * gamma(b)) * mu.^(a-1.0) .* (1.0 .- mu).^(b-1.0)
+end
+
 end # module
