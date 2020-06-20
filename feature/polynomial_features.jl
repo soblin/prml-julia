935e7d514df5f2595d4950ece45929f96749da95
soblin <hilo.soblin@gmail.com>
2020-06-20 22:22:04 +0900

    [feature] polynomial_features.jl

--- feature/polynomial_features.jl
+++ feature/polynomial_features.jl
@@ -2,15 +2,15 @@ struct PolynomialFeature
     _degree::Int64
 end
 
-function transform(feature::PolynomialFeature, x)
+function transform(feature::PolynomialFeature, x::Array{Float64, 1})
+    # return [phi(x_1), phi(x_2),,, phi(x_N)]
+    # phi(x_i) is the transfomed feature vector(vertical)
     sample_size = size(x)[1];
     degree = feature._degree;
     ret = zeros(Float64, degree+1, sample_size);
-    for i in 1:(degree+1)
-        ret[i, :] = [ j^(i-1) for j in x];
+    for i in 1:sample_size
+        ret[:, i] = [ x[i]^(j+1) for j in 0:degree];
     end
 
-    tmp = transpose(ret);
-    ret = collect(reshape(tmp, sample_size, degree+1));
     return ret;
 end
--- linear/linear_regressor.jl
+++ linear/linear_regressor.jl
@@ -6,20 +6,20 @@ mutable struct LinearRegressor
 end
 
 function fitting(regressor::LinearRegressor, Phi::Array{Float64, 2}, t::Array{Float64, 1})
-    # Phi is the design matrix of size (sample_size, n_feature)
-    # t is the target vector of size (sample_size)
+    # Phi is the transpose of design matrix of size (n_feature, sample_size)
+    # t is the target vector of size (sample_size, 1)
     # `pinv(Phi)` = (Phi^T * Phi)^(-1) * Phi^T
-    w = pinv(Phi) * t
-    error = Phi * w - t
+    w = pinv(transpose(Phi)) * t
+    error = transpose(Phi) * w - t
     var = sum(error.^2)  / size(t)[1]
     
     regressor._w = w
     regressor._var = var
 end
 
-function predict(regressor::LinearRegressor, phi, return_std::Bool)
-    # `phi` is the transformed feature of x, of size (sample_size, n_feature)
-    y = phi * regressor._w
+function predict(regressor::LinearRegressor, Phi::Array{Float64, 2}, return_std::Bool)
+    # `phi` is the transformed feature of x, of size (n_feature, sample_size)
+    y = transpose(Phi) * regressor._w;
     std = zeros(1, size(y)[1]) .+ regressor._var
     if return_std == true
         return y, std
