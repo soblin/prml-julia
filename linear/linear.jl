32f76115182e20898cbf300a470a32ebbb971e73
soblin <hilo.soblin@gmail.com>
2020-06-23 01:20:23 +0900

    [ch3] finished

--- linear/linear.jl
+++ linear/linear.jl
@@ -1,6 +1,7 @@
 export LinearRegressor
 export RidgeRegressor
 export BayesianRegressor
+export EmpiricalBayesianRegressor
 export fitting
 export predict
 export predictSampling
@@ -8,3 +9,4 @@ export predictSampling
 include("linear_regressor.jl")
 include("ridge_regressor.jl")
 include("bayesian_regressor.jl")
+include("empirical_bayesian_regressor.jl")
